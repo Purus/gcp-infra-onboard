d759f77166661b028499b466bd1cb1c1789fd8f6
ajinkya kolhe <ajinkyakolhe112@gmail.com>
2018-09-20 13:26:28 +0530

    changes to kubernetes cluster name

--- Kubernetes/1 create kub cluster.sh
+++ Kubernetes/1 create kub cluster.sh
@@ -2,4 +2,4 @@
 
 export MY_ZONE=us-central1-f
 
-gcloud container clusters create webfrontend --zone $MY_ZONE --num-nodes 2
+gcloud container clusters create kub-cluster --zone $MY_ZONE --num-nodes 2
